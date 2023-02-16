d6f406651a5ce0d7ba0abb4d5c8736423165f66c
Jddelara01 <jddelara01@gmail.com>
2023-02-16 12:54:12 +0000

    Add makeshift database using flask

--- run.py
+++ run.py
@@ -18,6 +18,18 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    # open json file for read only access
+    with open("data/company.json", "r") as json.data:
+        data = json.load(json.data)  # covert data into json
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
